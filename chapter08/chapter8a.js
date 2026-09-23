6731de5d278049acb8e581da7161671890de38d3
sckorpio <kartik.sckorpio@gmail.com>
2026-09-23 16:29:51 +0530

    Chapter8: 2D transformations

---
+++ chapter08/chapter8a.js
@@ -0,0 +1,266 @@
+/* #############################################################
+CHAPTER 8a: 2D Transformations
+
+Topics:
+- Translations basics
+###############################################################
+*/
+
+// =============================================================
+// 1. GLSL Shader Sources
+// =============================================================
+
+const vertexShaderSource = `#version 300 es
+    in vec2 a_position;
+    uniform mat3 u_projection;
+    uniform mat3 u_view;
+    uniform mat3 u_model;
+
+    void main() {
+        gl_Position = vec4((u_projection * u_view * u_model * vec3(a_position, 1.0)).xy, 0.0, 1.0);
+    }
+`;
+
+const fragmentShaderSource = `#version 300 es
+    precision mediump float;
+    out vec4 out_color;
+
+    void main() {
+        out_color = vec4(0.39, 0.33, 0.58, 1.0);    // Sckorpio-Purple
+    }
+`;
+
+function createShader(gl, type, source) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    const compileStatus = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (compileStatus) return shader;
+
+    console.error("Shader Compilation Error:", gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    const linkStatus = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (linkStatus) return program;
+
+    console.error("Program Linking Error:", gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+// =============================================================
+// 2. Helper Functions
+// =============================================================
+
+function resizeCanvasToDisplaySize(canvas, multiplier = 1) {
+    const width = (canvas.clientWidth * multiplier) | 0;
+    const height = (canvas.clientHeight * multiplier) | 0;
+
+    if (canvas.width !== width || canvas.height !== height) {
+        canvas.width = width;
+        canvas.height = height;
+        return true;
+    }
+    return false;
+}
+
+// =============================================================
+// 0. GUI using.. lil-gui
+// =============================================================
+
+var state = {
+    // Vertices
+    aX: 100, aY: 0,
+    bX: 50, bY: 50,
+    cX: 50, cY: 150
+};
+
+function setupGUI(canvas, render) {
+    const gui = new lil.GUI();
+    const verticesFolder = gui.addFolder("Vertices");
+    // Point A
+    const pointAFolder = verticesFolder.addFolder("Point A");
+    pointAFolder.add(state, "aX", 0, canvas.width).name("X").onChange(render);
+    pointAFolder.add(state, "aY", 0, canvas.height).name("Y").onChange(render);
+    // Point B
+    const pointBFolder = verticesFolder.addFolder("Point B");
+    pointBFolder.add(state, "bX", 0, canvas.width).name("X").onChange(render);
+    pointBFolder.add(state, "bY", 0, canvas.height).name("Y").onChange(render);
+    // Point C
+    const pointCFolder = verticesFolder.addFolder("Point C");
+    pointCFolder.add(state, "cX", 0, canvas.width).name("X").onChange(render);
+    pointCFolder.add(state, "cY", 0, canvas.height).name("Y").onChange(render);
+}
+
+
+// =============================================================
+// 3. Main Application Entry Point
+// =============================================================
+
+function main() {
+    // -------------------------------------------------------------
+    // 1. WEBGL CANVAS
+    // -------------------------------------------------------------
+    // canvas
+    const canvas = document.querySelector("#c");
+    if (!canvas) {
+        console.error("Canvas element not found");
+        return;
+    }
+    // context
+    const gl = canvas.getContext("webgl2");
+    if (!gl) {
+        console.error("WebGL2 is not supported by this browser");
+        return;
+    }
+
+    // -------------------------------------------------------------
+    // 2. SHADERS
+    // -------------------------------------------------------------
+    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+    const program = createProgram(gl, vertexShader, fragmentShader);
+
+    // Save Attribute & Uniform locations
+    const locationAttributePosition = gl.getAttribLocation(program, "a_position");
+    const locationUniformProjection = gl.getUniformLocation(program, "u_projection");
+    const locationUniformView = gl.getUniformLocation(program, "u_view");
+    const locationUniformModel = gl.getUniformLocation(program, "u_model");
+    const locationUniformColor = gl.getUniformLocation(program, "u_color");
+
+    // -------------------------------------------------------------
+    // 3. DATA & BUFFERS
+    // -------------------------------------------------------------
+    // VERTEX BUFFER
+    const vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    // data to be filled by UI...
+
+    // -------------------------------------------------------------
+    // 4. VERTEX ARRAY
+    // -------------------------------------------------------------
+
+    // vao: vertex array object
+    const vao = gl.createVertexArray();
+
+    // bind the vertex array
+    gl.bindVertexArray(vao);
+
+    // enable the position attribute
+    gl.enableVertexAttribArray(locationAttributePosition);
+
+    // bind the vertex buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+
+    // Buffer data format
+    const size = 2;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+
+    gl.vertexAttribPointer(
+        locationAttributePosition,
+        size,
+        type,
+        normalize,
+        stride,
+        offset
+    );
+
+    // =============================================================
+    // 6. RENDER
+    // =============================================================
+
+    function render() {
+        // CANVAS
+        resizeCanvasToDisplaySize(gl.canvas);
+        // GUI
+        setupGUI(canvas, render);
+
+        // ViewPort
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        // BACKGROUND
+        //gl.clearColor(0.39, 0.33, 0.58, 1.0); //Sckorpio-Purple
+        gl.clearColor(0.32, 0.63, 0.67, 1.0);  //Sckorpio-Cyan  
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // SHADER
+        gl.useProgram(program);
+
+        // BUFFER / DATA
+        // The VAO remembers the vertex attribute configuration.
+        gl.bindVertexArray(vao);
+
+        // Vertex data CPU side
+        const positions = new Float32Array([
+            // Vertices
+            state.aX, state.aY, // point 1
+            state.bX, state.bY, // point 2
+            state.cX, state.cY  // point 3
+        ]);
+        //Feed the vertex data to buffer GPU
+        gl.bufferData(
+            gl.ARRAY_BUFFER, // bind point
+            positions,       // cpu data
+            gl.DYNAMIC_DRAW   // how frequent we gonna use it (STATIC/DYNAMIC)
+        );
+
+        // ---------------------------------------------------------
+        // TRANSFORMATION MATRIX
+        // ---------------------------------------------------------
+        // projection matrix
+        const projection = mat3.create(); // Identity
+        // view matrix
+        const view = mat3.fromValues(
+             2 / gl.canvas.width,   0,                      0,
+             0,                     -2 / gl.canvas.height,  0,
+            -1,                     1,                      1
+        );
+        // model matrix
+        const model = mat3.create();
+
+        // Upload transformation matrix
+        gl.uniformMatrix3fv(locationUniformProjection,false,projection);
+        gl.uniformMatrix3fv(locationUniformView,false,view);
+        gl.uniformMatrix3fv(locationUniformModel,false,model);
+
+        // Color 
+        gl.uniform4fv(locationUniformColor,[0.39, 0.33, 0.58, 1.0]); // Purple
+
+
+        // ---------------------------------------------------------
+        // DRAW CALL
+        // ---------------------------------------------------------
+
+        const draw_primitiveType = gl.TRIANGLES;
+        const draw_offset = 0;
+        const draw_count = 3;
+
+        gl.drawArrays(
+            draw_primitiveType,
+            draw_offset,
+            draw_count
+        );
+    }
+
+    render();
+
+    // Listen for Window resize
+    window.addEventListener("resize", render);
+}
+
+// Start app once DOM content is ready
+window.addEventListener("DOMContentLoaded", main);
+
+export {
+    main
+};
